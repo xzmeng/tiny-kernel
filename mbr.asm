97f2fa3b7e838afc5ff8c95ef17fe98f81a950ad
Meng Xiangzhuo <aumo@foxmail.com>
2021-01-13 10:02:07 +0800

    hello,world via directly writing to video memory.

--- mbr.asm
+++ mbr.asm
@@ -1,10 +1,13 @@
-section dummy vstart=0x7c00
+org 0x7c00
 
-; set ss: sp
+; set ss: sp and es
 mov ax, cs
 mov ss, ax
-mov es, ax
 mov sp, 0x7c00
+
+; set the es as the start of video memory(text mode)
+mov ax, 0xb800
+mov es, ax
 
 
 ;-------clear screen--------
@@ -17,18 +20,12 @@ mov dl, 79 ; right-down col
 int 10h
 
 ;-------print hello,world-------
-mov ah, 13h
-mov al, 00000001b ; bit 0: update cursor after writing
-mov bh, 0h ; page 0
-mov bl, 00001110b ; 0000 black bg, 1110 yellow font
-mov cx, 12 ; len(string)
-mov dx, 0 ; print start from (0, 0)
-mov bp, string
-int 0x10
+mov byte [es:00h], 'h'
+mov byte [es:01h], 00001110b ;blak bg; yellow font
+mov byte [es:02h], 'w'
+mov byte [es:03h], 00001110b ;blak bg; yellow font
 
 jmp $
-
-string db "hello,world!"
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
